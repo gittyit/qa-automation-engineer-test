78e69702f4257d13283648d37f1651c0c67f08d0
vitaliimr <vitaliimr@gmail.com>
2023-11-07 13:21:52 +0000

    types + pylint + venv

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-import asyncpg
 import asyncio
+import asyncpg
 import os
-# import logging as Log
 
-from flask import Flask as Fl, url_for
+from asyncpg.connection import Connection
+from flask import Flask as Fl
 from flask import render_template as Render
 from flask import jsonify
 
@@ -19,16 +19,16 @@ async def result():
     - In case of success returns data in JSON format
     '''
     # Get a new connection to the DB
-    msg, Ex, cn = await get_connection()
+    msg, ex, cn = await get_connection()
     # In case of errors --> Display error page
     if not cn:
-        return Render("error.html", error_msg=msg, ex_msg=Ex)
+        return Render("error.html", error_msg=msg, ex_msg=ex)
 
     try:
         # Retrieve all data from 3 tables with timeout 2 seconds
         d = await asyncio.wait_for(retrieve_data(cn), timeout=2)
-    except BaseException as Ex:
-        msg, ex_str = add_msg("Can't retrieve data from DB tables", Ex)
+    except TimeoutError as ex:
+        msg, ex_str = add_msg("Can't retrieve data from DB tables", ex)
         return Render("error.html", error_msg=msg, ex_msg=ex_str)
     return jsonify(d)
 
@@ -44,9 +44,6 @@ async def run():
     - In case of error returns an error page with error details
     - In case of success returns success message
     '''
-    # Log.basicConfig(level=Log.ERROR)
-    # Log.info('Run method started')
-    # url = url_for('run')
     # Get a new connection to the DB
     msg, ex_str, cn = await get_connection()
     # In case of errors --> Display error page
@@ -58,8 +55,8 @@ async def run():
         await cn.execute('''
             DROP TABLE IF EXISTS data_1, data_2, data_3;
         ''')
-    except BaseException as Ex:
-        msg, ex_str = add_msg("Can't delete DB tables", Ex)
+    except BaseException as ex:
+        msg, ex_str = add_msg("Can't delete DB tables", ex)
         return Render("error.html", error_msg=msg, ex_msg=ex_str)
 
     try:
@@ -78,8 +75,8 @@ async def run():
                 name VARCHAR(255)
             );
         ''')
-    except BaseException as Ex:
-        msg, ex_str = add_msg("Can't create DB tables", Ex)
+    except BaseException as ex:
+        msg, ex_str = add_msg("Can't create DB tables", ex)
         return Render("error.html", error_msg=msg, ex_msg=ex_str)
 
     await populate_tables(cn)
@@ -91,7 +88,7 @@ async def run():
     return f"<p>{message}</p>"
 
 
-async def get_connection():
+async def get_connection() -> Connection:
     try:
         # Establish a connection to a DB named "postgres"
         pw = os.environ.get('POSTGRES_PASSWORD')
@@ -100,16 +97,16 @@ async def get_connection():
         if not (pw and host and port):
             raise Exception("Provide environment vars to connect to the DB")
         cn = await asyncpg.connect(f'postgresql://postgres:{pw}@{host}:{port}/postgres')
-    except BaseException as Ex:
-        msg, ex_str = add_msg("Can't connect to the DB", Ex)
+    except BaseException as ex:
+        msg, ex_str = add_msg("Can't connect to the DB", ex)
         return msg, ex_str, ''
     return '', '', cn
 
 
-async def retrieve_data(connection):
+async def retrieve_data(conn: Connection) -> list:
     d = []
-    async with connection.transaction():
-        async for rec in connection.cursor('''
+    async with conn.transaction():
+        async for rec in conn.cursor('''
             SELECT * FROM data_1
             UNION ALL
             SELECT * FROM data_2
@@ -122,34 +119,34 @@ async def retrieve_data(connection):
     return d
 
 
-def add_msg(text, exception=''):
+def add_msg(text, exception='') -> (str, str):
     msg = f"{text}: {exception}"
     print(msg)
     return text, str(exception)
 
 
-async def populate_tables(connection):
+async def populate_tables(conn: Connection):
     # Populating the table data_1: IDs 1-10, 31-40;
-    await populate_table(connection, 'data_1', range(1, 11), range(31, 41))
+    await populate_table(conn, 'data_1', range(1, 11), range(31, 41))
 
     # Populating the table data_2: IDs 11-20, 41-50;
-    await populate_table(connection, 'data_2', range(11, 21), range(41, 51))
+    await populate_table(conn, 'data_2', range(11, 21), range(41, 51))
 
     # Populating the table data_3: IDs 21-30, 51-60;
-    await populate_table(connection, 'data_3', range(21, 31), range(51, 61))
+    await populate_table(conn, 'data_3', range(21, 31), range(51, 61))
 
 
-async def populate_table(connection, tablename, sequence1: None, sequence2: None):
+async def populate_table(conn: Connection, tab: str, sequence1=None, sequence2=None):
     try:
         # If sequence1 is defined --> Populate the table with it
         if sequence1:
             for id in sequence1:
-                await connection.execute(f"INSERT INTO {tablename} (id, name) VALUES({id}, 'Test {id}');")
+                await conn.execute(f"INSERT INTO {tab} (id, name) VALUES({id}, 'Test {id}');")
 
         # If sequence2 is defined --> Populate the table with it
         if sequence2:
             for id in sequence2:
-                await connection.execute(f"INSERT INTO {tablename} (id, name) VALUES({id}, 'Test {id}');")
-    except BaseException as Ex:
-        msg, ex_str = add_msg(f"Can't insert data into table {tablename}", Ex)
+                await conn.execute(f"INSERT INTO {tab} (id, name) VALUES({id}, 'Test {id}');")
+    except BaseException as ex:
+        msg, ex_str = add_msg(f"Can't insert data into table {tab}", ex)
         return Render("error.html", error_msg=msg, ex_msg=ex_str)
