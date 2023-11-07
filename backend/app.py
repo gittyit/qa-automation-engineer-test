468016d25b752f350418915c92e838397485c2a9
vitaliimr <vitaliimr@gmail.com>
2023-11-07 18:15:09 +0000

    async retrieval + docstrings

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
+'''  QA Automation Engineer Test:
+- Index page --> Creates DB tables and populates it (async)
+- Resul page --> Retrieves the data from DB tables in JSON (async)
+'''
+import os
 import asyncio
 import asyncpg
-import os
 
 from asyncpg.connection import Connection
 from flask import Flask as Fl
@@ -13,21 +17,22 @@ app = Fl(__name__)
 @app.route('/result')
 async def result():
     ''' Result page method:
-    - Queries all data from tables and sort it by ID.
-    - Creates a JSON response with retrieved data.
+    - Queries all data from tables concurrently and sort it by ID.
     - If no data is found returns an error page with error details
     - In case of success returns data in JSON format
     '''
-    # Get a new connection to the DB
-    msg, ex, cn = await get_connection()
-    # In case of errors --> Display error page
-    if not cn:
-        return Render("error.html", error_msg=msg, ex_msg=ex)
-
     try:
-        # Retrieve all data from 3 tables with timeout 2 seconds
-        d = await asyncio.wait_for(retrieve_data(cn), timeout=2)
-    except TimeoutError as ex:
+        data = []
+        # Create coroutines list with data queries from 3 tables
+        coros = [retrieve_data(i, data) for i in range(1, 4)]
+        # Run coroutines concurrently and get data with timeout 2 seconds
+        for co in asyncio.as_completed(coros, timeout=2):
+            await co
+        # Sort data by id
+        d = sorted(data, key=lambda x: x['id'])
+        print(d)
+        # d = await asyncio.wait_for(retrieve_data(cn), timeout=2)
+    except BaseException as ex:
         msg, ex_str = add_msg("Can't retrieve data from DB tables", ex)
         return Render("error.html", error_msg=msg, ex_msg=ex_str)
     return jsonify(d)
@@ -88,7 +93,13 @@ async def run():
     return f"<p>{message}</p>"
 
 
-async def get_connection() -> Connection:
+async def get_connection() -> (str, str, Connection):
+    ''' Gets a new DB connection with parameters from environment vars:
+    Returns 3 parameters:
+    - error message (empty if none)
+    - exception text (empty if none)
+    - connection object (empty if none)
+    '''
     try:
         # Establish a connection to a DB named "postgres"
         pw = os.environ.get('POSTGRES_PASSWORD')
@@ -103,29 +114,54 @@ async def get_connection() -> Connection:
     return '', '', cn
 
 
-async def retrieve_data(conn: Connection) -> list:
-    d = []
-    async with conn.transaction():
-        async for rec in conn.cursor('''
-            SELECT * FROM data_1
-            UNION ALL
-            SELECT * FROM data_2
-            UNION ALL
-            SELECT * FROM data_3
-            ORDER BY id;
-        '''):
-            d.append({"id": rec['id'],
-                      "name": rec['name']})
-    return d
+async def retrieve_data(i: int, d: list):
+    """Gets data from DB table data_i:
+    Args:
+        i (int): table number
+        d (list): list, where data will be appended
+    """
+    # d = []
+    # Get a new connection to the DB
+    msg, ex_str, cn = await get_connection()
+
+    rows = await cn.fetch(f'SELECT * FROM data_{i};')
+    for rec in rows:
+        d.append({"id": rec['id'],
+                  "name": rec['name']})
+
+ # Close the connection
+    await cn.close()
+
+    # d = [dict(row) for row in rows]
+    # async with conn.transaction():s
+    #     async for rec in conn.cursor(f'SELECT * FROM data_{i};'):
+    #         d.append({"id": rec['id'],
+    #                   "name": rec['name']})
+    # return d
 
 
-def add_msg(text, exception='') -> (str, str):
+def add_msg(text: str, exception='') -> (str, str):
+    """ Creates message from text and exception and
+    prints it.
+
+    Args:
+        text (str): Text of the message
+        exception (Exception): Optional exception object
+    Returns:
+        text: Text of the message
+        ex_str: Text of the exception
+    """
     msg = f"{text}: {exception}"
     print(msg)
     return text, str(exception)
 
 
 async def populate_tables(conn: Connection):
+    """ Populates asynchronously 3 tables from ranges.
+
+    Args:
+        conn (Connection): Existing DB connection
+    """
     # Populating the table data_1: IDs 1-10, 31-40;
     await populate_table(conn, 'data_1', range(1, 11), range(31, 41))
 
@@ -137,6 +173,17 @@ async def populate_tables(conn: Connection):
 
 
 async def populate_table(conn: Connection, tab: str, sequence1=None, sequence2=None):
+    """ Populate asynchronously DB table from arguments.
+
+    Args:
+        conn (Connection): Existing DB connection
+        tab (str): Table name to populate
+        sequence1 (sequence, optional): Range. Defaults to None.
+        sequence2 (sequence, optional): Range. Defaults to None.
+
+    Returns:
+        Render: Rendered template with error page
+    """
     try:
         # If sequence1 is defined --> Populate the table with it
         if sequence1:
